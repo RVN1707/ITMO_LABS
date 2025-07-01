0348e7b5ae860b5bda5a168098d1bfba0088c54a
RVN <valievruslan504@gmail.com>
2025-07-01 21:16:25 +0300

    Добавил всякое

---
+++ ОПД_1_СЕМ/ЛАБ_1/ЛабаОПД1.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+mkdir lab0
+cd lab0
+mkdir carvanha4
+mkdir litwick5
+mkdir vullaby6
+cd carvanha4
+mkdir smoochum
+mkdir githitelle
+mkdir shelgon
+echo "Способности Torrent Mind Oblivious Own
+> Tempo" > slowking
+echo "Развитые способности Analytic" > patrat
+echo "weidth=20.5
+> height=20.0 atk=6 def=7" > cherrim
+cd ..
+echo "Тип покемона PSYCHIC
+> NONE" > chimecho6
+echo "Развитые способности Light Metal Heavy
+> Metal" > metagross3
+echo "Тип покемона WATER NONE" > poliwag4
+cd litwick5
+echo "satk=4 sdef=5 spd=6" > ekans
+echo "Живет" > silcoon
+echo "Способности
+Torrent Mind Oblivious Own Tempo" > slowbro
+echo "Возможности Overland=6
+Jump=3 Power=4 cd=4 Egg Warmer=0 Firestarter=0 Glow=0
+Heater=0 Sinker=0" > magmar
+echo "Способности Supersonic Horn Attak Wa
+ter
+Pulse Flail Aqua Ring Fury Attack Waterfall Horn Drill Agillity Soak
+Megahorn" > seaking
+cd ..
+cd vullaby6
+echo "satk=6 sdef=6
+> spd=4" > ferretress
+echo "Ходы Air Cutter Bug Bite Disable Dou
+ble-Edge
+Foresight‡ Giga Drain Ominous Wind Roost Signal Beam Silver Wind‡
+Skill Swap Sleep Talk Snore String Shot Swift Tailwind Twister Zen
+Headbutt" > venomoth
+echo "Тип диеты Omnivore" > milotic
+echo "Тип диеты Ergovore
+Herbivore" > elekid
+echo "satk=7 sdef=4 spd=6" > wailmer
+mkdir venonat
+cd ..
+chmod u=rw, g=r chimecho6
+chmod 644 poliwag4
+chmod 311 litwick5
+chmod 736 vullaby6
+chmod 511 carvanha4
+chmod 111 carvanha4/slowking
+chmod 577 carvanha4/smoochum
+chmod 513 carvanha4/gothitelle
+chmod 555 carvanha4/shelgon
+chmod 046 carvanha4/patrat
+chmod 064 carvanha4/cherrim
+chmod 006 metagross3
+chmod 046 litwick5/ekans
+chmod 440 litwick5/silcoon
+chmod 640 litwick5/slowbro
+chmod 624 litwick5/magmar
+chmod 004 litwick5/seaking
+chmod 044 vullaby6/venomoth
+chmod 604 vullaby6/milotic
+chmod 044 vullaby6/elekid
+chmod 404 vullaby6/wailmer
+chmod 573 vullaby6/venonat
+ln metagross3 ./litwick5/magmarmetagross
+chmod 777 vullaby6/venomoth
+cat ./vullaby6/wailmer ./vullaby6/venomoth > ./chimecho6_34
+chmod 044 vullaby6/venomoth
+ln -s poliwag4 ./litwick5/ekanspoliwag
+ln -s ./vullaby6 Copy_32
+cp -R vullaby6 lab0/carvanha4/shelgon 2>/dev/null
+chmod 706 metagross3
+cp metagross3 ./vullaby6/elekidmetagross 
+chmod 006 metagross3
+chmod 710 chimecho6
+chmod 773 vullaby6/venonat
+cp chimecho6 ./vullaby6/venonat
+chmod 610 chimecho6
+chmod 573 vullaby6/venonat
+wc -m poliwag4 > ./tmp 2>&1
+ls -lR . | grep '^c'|sort -k2n
+cat chimecho6 | grep -ni 'o$'
+ls -lR lab0 . | grep '^.* m' | head -n 2
+ls -lt . /s* | head -n 4
+chmod 711 litwick5
+ls -R litwick5 | sort -r
+chmod 311 litwick5
+rm -f metagross3
+rm ./vullaby6/ferretress
+chmod 711 litwick5
+rm ./litwick5/ekanspoliw*
+chmod 311 litwick5
+chmod 711 litwick5
+rm -f ./litwick5/magmarmetagro*
+chmod 311 litwick5
+chmod 711 carvanha4
+chmod 764 carvanha4/cherrim
+chmod 755 carvanha4/shelgon
+chmod 777 carvanha4/smoochum
+chmod 713 carvanha4/githitelle
+chmod 711 carvanha4/slowking
+chmod 746 carvanha4/patrat
+rm -r ./carvanha4
+
+
+
+
+		

c3b919d661fc7c14fd8554ef7c1f55266e043d3a
RVN <valievruslan504@gmail.com>
2025-08-13 01:06:32 +0300

    Добавил проги

---
+++ ПРОГ_1_СЕМ/ЛАБ_1.java
@@ -0,0 +1,21 @@
+package org.example;
+public class My_1_Lab {
+    public static void main(String[] args) {
+        short[] s = new short[16];
+        double[] x = new double[10];
+        double[][] m = new double[16][10];
+        for (int i = 0; i < s.length; i++) {
+            switch(s[i]){
+                case 5:
+                    for (int i = 0; i < s.length; i++) {
+
+                    }
+                case 8, 9, 10, 11, 12, 14, 15, 19:
+                default:
+            }
+            for (double valueX : x) {
+                switch (value)
+            }
+        }
+    }
+}
